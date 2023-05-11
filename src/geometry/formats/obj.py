79beea8265ee1f43dbe6a252e9a2b46514a8e30c
Louis Novy <louis@kavsports.com>
2023-05-11 08:16:58 -0700

    terrible obj loader for the time being

---
+++ src/geometry/formats/obj.py
@@ -0,0 +1,52 @@
+import numpy as np
+from .. import mesh
+from ..utils import polygons_to_triangles
+# TODO: faster parsing?
+
+extensions = [".obj"]
+
+def save(path: str, mesh):
+    with open(path, "w") as f:
+        colors = mesh.vertices.colors
+        if colors is not None:
+            for v, c in zip(mesh.vertices, colors):
+                f.write(f"v {v[0]} {v[1]} {v[2]} {c[0]} {c[1]} {c[2]}\n")
+        else:
+            for v in mesh.vertices:
+                f.write(f"v {v[0]} {v[1]} {v[2]}\n")
+
+        for face in mesh.faces:
+            f.write(f"f {face[0] + 1} {face[1] + 1} {face[2] + 1}\n")
+
+def load(path: str):
+    with open(path) as f:
+        vertices = []
+        faces = []
+        colors = []
+        for line in f:
+            if line.startswith("v "):
+                v = line.split()[1:]
+                if len(v) == 6:
+                    vertices.append(v[:3])
+                    colors.append(v[3:])
+                else:
+                    vertices.append(v)
+            elif line.startswith("f "):
+                faces.append(line.split()[1:])
+
+    # convert faces from sets of / delimited indices to sets of space delimited indices
+    if any("/" in face for face in faces):
+        faces = [[x.split("/")[0] for x in face] for face in faces]
+    
+    # convert faces to triangles
+    faces = polygons_to_triangles(faces)
+    vertices = np.array(vertices, dtype=float)
+    faces = np.array(faces, dtype=int) - 1
+
+    # if colors:
+    #     return mesh.TriangleMesh(vertices, faces, vertex_colors=np.array(colors, dtype=float))
+
+    # return mesh.TriangleMesh(vertices, faces)
+
+    return vertices, faces
+
