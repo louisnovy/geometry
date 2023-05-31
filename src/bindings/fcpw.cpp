95b3b058cb78fc200b76a5c148add149953b4b73
Louis Novy <louis@kavsports.com>
2023-05-31 10:46:25 -0700

    add currently empty fcpw bindings file

---
+++ src/bindings/fcpw.cpp
@@ -0,0 +1,9 @@
+#include <fcpw/fcpw.h>
+#include <pybind11/eigen.h>
+#include <pybind11/functional.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <string>
+
+namespace py = pybind11;
+
